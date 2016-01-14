91f81b71db34c406aab06b1b6cab5e261b4dbee4
jrjacobson <jrjacobson86@gmail.com>
2016-01-14 10:41:37 -0700

    swaped out my .html for a .php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+	<p>Hello World</p>
+<?php
+for ($num = 0; $num < 10; $num++ ){
+	echo "<div>This is div tag $num </div>";
+}
+?>
+</body>
+</html>
