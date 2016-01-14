a63b18f6792f3d38c4c3264710734fa50f96a7c2
Jason <jrjacobson86@gmail.com>
2016-01-14 15:46:32 -0700

    just started working on my homepage and wanted to commit that

--- index.php
+++ index.php
@@ -1,14 +1,54 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title></title>
-</head>
-<body>
-	<p>Hello World</p>
-<?php
-for ($num = 1; $num <= 10; $num++ ){
-	echo "<div>This is div tag $num </div>";
-}
-?>
-</body>
+  <head>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  <title>Jason's Homepage</title>
+  </head>
+
+  <body>
+    <div class="container">
+
+      <ul class="nav nav-pills" role="tablist">
+        <li role="presentation" class="active"><a href="index.php">Home</a></li>
+        <li role="presentation"><a href="hello.php">HelloWorld</a></li>
+        <li role="presentation"><a href="#">Messages</a></li>
+      </ul>
+
+      <div class = "jumbotron">
+        <div class="container">
+          <h1>Welcome to Jason's Homepage</h1>
+          <p>This hompage is mostly for demonstration and will be used to
+            demonstrate mainly the use of server side php.</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-8">
+          <p>
+            This is my wonderful webpage I will do my best to awh and amaze
+            you. First I want to start off by introducing myself.
+            My name is Jason Jaocbson I am a Software Engineering major
+            here at BYU-Idaho. I <strong>love</strong> to write code and goof
+            around on my computer. Strangely I also love to run two summers ago
+            I ran my first two marathons and last summer I competed in my first
+            olimpic distance triatholon. When I have free time I like to get out
+            and do any type of training I can.
+          </p>
+        </div>
+        <div class="col-md-4">
+          <a href="#" class="thumbnail">
+            <img src="jasonTraciMesaFalls.jpg" alt="Traci and Jason at mesa falls marathon">
+          </a>
+        </div>
+      </div>
+
+    </div>
+  </body>
 </html>
