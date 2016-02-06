92460a68ad5075667031d5003b90ed3f481a4dd0
jrjacobson <jrjacobson86@gmail.com>
2016-02-05 21:18:02 -0700

    adding some stuff to the DB

--- displayingDB.php
+++ displayingDB.php
@@ -13,20 +13,9 @@
     $dbUser = "jrjaco86";
     $dbPassword = "help123";
     $dbName = "workoutlog";
-    */
-    define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-    define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-    define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-    define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-    define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
-
-    $dbHost = constant("DB_HOST"); // Host name
-    $dbPort = constant("DB_PORT"); // Host port
-    $dbUser = constant("DB_USER"); // Mysql username
-    $dbPassword = constant("DB_PASS"); // Mysql password
-    $dbName = constant("DB_NAME"); // Database name
-
     $db = new PDO('mysql:host=dbHost:$dbPort;dbname=$dbName', $dbUser, $dbPassword);
+    */
+    $db = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "", getenv('OPENSHIFT_MYSQL_DB_PORT')) or die("Error: " . mysqli_error($mysqlCon));
 
     //working with the database
     $MAX_WORKOUTS = 1000;
