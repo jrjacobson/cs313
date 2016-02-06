2faf236575405f74d884ec33ac3635ae00ed1674
jrjacobson <jrjacobson86@gmail.com>
2016-02-05 20:18:08 -0700

    adding some stuff to the DB

--- displayingDB.php
+++ displayingDB.php
@@ -25,9 +25,10 @@
     else
     {
       $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-      $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+      $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
       $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
       $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+      $dbName = "php";
     }
     $db = new PDO('mysql:host=dbHost:$dbPort;dbname=$dbName', $dbuser, $dbpassword);
 
