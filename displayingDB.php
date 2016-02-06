142934c0204b9fedf78f1e8abe86c48e65700409
Jason <jrjacobson86@gmail.com>
2016-02-05 22:09:49 -0700

    Trying to get the db working

--- displayingDB.php
+++ displayingDB.php
@@ -19,6 +19,7 @@
     $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
     $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
     $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+    $dbName = "php";
     $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
     //working with the database
