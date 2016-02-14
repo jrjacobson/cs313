b5d60486690f39cf13028a09399e5856065daa78
Jason <jrjacobson86@gmail.com>
2016-02-13 21:37:01 -0700

    Okay I fixed some problems that where holding the data from being put into the table.

--- addToLog.php
+++ addToLog.php
@@ -1,34 +1,17 @@
-<?php session_start();?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Log Confirmation</title>
-</head>
-<body>
-
-<p>You input the following information.<br>
 <?php
-    echo 'Sport: '.$_POST["sport"].'<br>Weather: '.$_POST["weather"].'<br>Temperature: '.$_POST["temp"].'<br>';
-    echo 'Date: '.$_POST["date"].'<br>Distance: '.$_POST["distance"].'<br>Duration: '.$_POST["duration"].'<br>';
-    echo 'Journal: '.$_POST["journal"];
-?>
-</p>
-    <?php
+    session_start();
     require "dbObject.php";
     $user = $_SESSION["userId"];
     $sport = $_POST["sport"];
-    $weather = $_POST["weather"] + 21;
-    $temp = $_POST["temp"];
+    $weather = $_POST["weather"];
+    $temp = $_POST["temp"] + 21;
     $date = $_POST["date"];
     $dist = $_POST["distance"];
     $dur = $_POST["duration"];
     $journal = $_POST["journal"];
-    $sql = "INSERT INTO workout (userId, sportId, weatherId, tempId, date, distance, duration, journal) values ('".$user."', '".$sport."', '".$weather."', '".$temp."', '".$date."', '".$journal."')";
+    $sql = "INSERT INTO workout (userId, sportId, weatherId, tempId, date, distance, duration, journal) values (".$user.", ".$sport.", ".$weather.", ".$temp.", '".$date."', ".$dist.", ".$dur.", '".$journal."')";
+    echo $sql;
     $db->exec($sql);
-
-    //header("Location:workoutLogbook.php");
-    ?>
-<button><a href="workoutLogbook.php">Back to Log</a></button>
-</body>
-</html>
+    header("Location:workoutLogbook.php");
+?>
 
