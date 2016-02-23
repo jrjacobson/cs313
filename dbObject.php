00549174ea6350e4b16761e36af1a07fb4fcd9fd
jrjacobson <jrjacobson86@gmail.com>
2016-02-22 22:02:21 -0700

    using php to fill in some data in my empty database

--- dbObject.php
+++ dbObject.php
@@ -31,25 +31,9 @@ catch (PDOException $ex)
    die();
 }
 
-for($i = -20; $i < 111; $i++)
-{
-  $sql = "INSERT INTO temp (temp) values ($i)";
-  $db->exec($sql);
-}
-    $sql = "INSERT INTO sport (title) values ('Run')";
-    $sql += "INSERT INTO sport (title) values ('Bike')";
-    $sql += "INSERT INTO sport (title) values ('Swim')";
-    $sql += "INSERT INTO weather (title) values ('Sunny')";
-    $sql += "INSERT INTO weather (title) values ('Windy')";
-    $sql += "INSERT INTO weather (title) values ('Rainy')";
-    $sql += "INSERT INTO weather (title) values ('Cloudy')";
-    $sql += "INSERT INTO weather (title) values ('Snowy')";
-$db->exec($sql);
-    $sql = "INSERT INTO user (userEmail, displayName, password) values ('jrjacob@gmail.com', 'Jason Jacobson', 'hello')";
-$db->exec($sql);
-    $sql = "INSERT INTO workout (userId, sportId, weatherId, tempId, date, distance, duration, journal) values (1, 1, 1, 35, '2016-02-02 13:43:00', 5, 40, 'I had a great run!')";
-    $sql += "INSERT INTO workout (userId, sportId, weatherId, tempId, date, distance, duration, journal) values (1, 1, 1, 40, '2016-03-02 13:43:00', 6, 47, 'Another great run!')";
-    $db->exec($sql);
+/*
+ *
+ */
 
 
 ?>
